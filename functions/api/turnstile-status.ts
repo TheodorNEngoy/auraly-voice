a88d6d13bdc4dc381515781dfe5487f3777187b5
TheodorNEngoy <engoy.theodor@gmail.com>
2025-10-25 03:36:44 +0200

    chore: remove debug endpoints

--- functions/api/turnstile-status.ts
+++
@@ -1,5 +0,0 @@
-﻿export const onRequestGet: PagesFunction = async (ctx) => {
-  const siteKey = (ctx.env as any).NEXT_PUBLIC_TURNSTILE_SITE_KEY || null;
-  const hasSecret = !!(ctx.env as any).TURNSTILE_SECRET_KEY;
-  return new Response(JSON.stringify({ siteKey, hasSecret }), { headers: { "Content-Type": "application/json", "Cache-Control": "no-store" } });
-};
